0e28ea2b30ba36bb912ce1f8a7eb613c6d3b7110
Navee <raozhanghui@gmail.com>
2014-05-20 17:17:40 +0800

    增加配置文件

--- src/main/java/org/navee/springboot/sample/Simple.java
+++ src/main/java/org/navee/springboot/sample/Simple.java
@@ -2,18 +2,11 @@ package org.navee.springboot.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
-@RestController
+@ComponentScan
 @EnableAutoConfiguration
 public class Simple {
-	
-	@RequestMapping("/")
-	String home(){
-		return "Hello World";
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Simple.class, args);
 	}
---
+++ src/main/java/org/navee/springboot/sample/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.navee.springboot.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	String home(){
+		return "Hello World";
+	}
+}
