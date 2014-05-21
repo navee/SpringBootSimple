2ca2125e7f810290f1c6dac02f964f7bd4a7e353
Navee <raozhanghui@gmail.com>
2014-05-21 22:16:58 +0800

    删除继承

--- src/main/java/org/navee/springboot/sample/SimpleApplication.java
+++ src/main/java/org/navee/springboot/sample/SimpleApplication.java
@@ -2,14 +2,13 @@ package org.navee.springboot.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class SimpleApplication extends SpringBootServletInitializer{
+public class SimpleApplication{
 	
 	public static void main(String[] args){
 		SpringApplication.run(SimpleApplication.class, args);
